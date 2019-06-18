025938bba6fe8dd83c397b8b63d2ff7222111a33
bgantick <brian@punkave.com>
2019-06-18 15:00:37 -0400

    Dismiss functionality

--- src/js/site.js
+++ src/js/site.js
@@ -1,5 +1,19 @@
+import { helpers, classes } from 'pk-utilities';
+
 function init () {
-  console.log('init');
+  const alert = '[data-component="pk-alert"]';
+  const confirm = '[data-role="dismiss-pk-alert"]';
+  let i;
+  for (i = 0; i < document.querySelectorAll(alert).length; ++i) {
+    helpers.addClass(document.querySelectorAll(alert)[i], classes.active);
+  }
+  for (i = 0; i < document.querySelectorAll(confirm).length; ++i) {
+    const button = document.querySelectorAll(confirm)[i];
+    const scopedAlert = helpers.closest(button, alert);
+    button.addEventListener('click', function () {
+      helpers.removeClass(scopedAlert, classes.active);
+    });
+  }
 };
 
 init();
