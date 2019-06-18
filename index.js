64a2dabd0343a23feccfc963169b2531aac54bd8
bgantick <brian@punkave.com>
2019-06-18 11:26:50 -0400

    Just render the template

--- index.js
+++ index.js
@@ -2,12 +2,19 @@ module.exports = {
   extend: 'apostrophe-module',
   moogBundle: {
     modules: [
-      'pk-cookie-alert-global',
-      'pk-cookie-alert-widgets'
+      'pk-cookie-alert-global'
     ],
     directory: 'lib/modules'
   },
+  construct: (self, options) => {
+    self.prependAlert = () => {
+      self.apos.templates.prepend('body', (req) => {
+        return self.partial('alert');
+      });
+    };
+  },
   afterConstruct: (self) => {
+    self.prependAlert();
     self.pushAsset('script', 'always', { when: 'always' });
     self.pushAsset('stylesheet', 'always', { when: 'always' });
   }
--- src/js/site.js
+++ src/js/site.js
@@ -1,13 +1,5 @@
 function init () {
-  console.log('this works');
-  apos.define('pk-cookie-alert-widgets', {
-    extend: 'apostrophe-widgets',
-    construct: function (self, options) {
-      self.play = function ($widget, data, options) {
-        console.log('this doesn\'t work');
-      };
-    }
-  });
+  console.log('init');
 };
 
 init();
